0d1dd241d766a9f0447cee40572ddc986b8a09f4
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-03 19:41:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Dashboard from './components/Dashboard';
+
+function Home() {
+  return <h1>Welcome to Home!</h1>;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
