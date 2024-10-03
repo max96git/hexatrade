adf3f491486f7d17046760ecefec8c0101fe15a2
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-03 17:24:22 +0300

    Create AccountListingLoader.jsx

---
+++ src/components/AccountListingLoader.jsx
@@ -0,0 +1,10 @@
+// AccountListingLoader.jsx
+import React from 'react';
+
+const AccountListingLoader = () => (
+  <div style={{ fontSize: '24px', marginTop: '20px' }}>
+    Loading...
+  </div>
+);
+
+export default AccountListingLoader;
