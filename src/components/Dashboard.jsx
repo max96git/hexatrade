2e541c4ada0fea4714071392346f882ad2c863bd
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-03 19:22:41 +0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 // Dashboard.jsx
 import React, { useState } from 'react';
-import { db } from './firebase';
+import { db } from '../firebase';
 import { collection, addDoc } from 'firebase/firestore';
 
 const Dashboard = () => {
